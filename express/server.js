b6012934451c772c630ebd3785b3c84389e7878a
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-06 16:25:44 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../Login.js')));
-app.post('/login',, (req, res) => {
+app.post('/login', (req, res) => {
   res.send({
     token: 'test123'
   });
