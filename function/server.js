844ae952c3a1bac61f4caf8661ac49c5a89e6206
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-06 15:37:24 +0700

    Add files via upload

--- function/server.js
+++ function/server.js
@@ -1,13 +1,24 @@
-const express = require('express');
-const cors = require('cors')
-const app = express();
+"use strict"
+const express = require("express")
+const serverless = require("serverless-http")
+const app = express()
+const bodyParser = require("body-parser")
+const router = express.Router()
 
-app.use(cors());
+app.use(bodyParser.json())
+app.use("/.netlify/functions/server", router) // path must route to lambda
+app.use("/", router)
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
+
+router.use('/login', (req, res) => {
+    res.send({
+      token: 'test123'
+    });
   });
-});
-
+  
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+  
+
+
+module.exports = app
+module.exports.handler = serverless(app)
