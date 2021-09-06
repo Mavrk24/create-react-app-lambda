4064620e2ad8af20fadf29205ff2a4b254b2de22
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-06 16:31:11 +0700

    Add files via upload

--- express/server.js
+++ express/server.js
@@ -3,10 +3,9 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
+const router = express.Router();
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../Login.js')));
 app.post('/login', (req, res) => {
   res.send({
     token: 'test123'
