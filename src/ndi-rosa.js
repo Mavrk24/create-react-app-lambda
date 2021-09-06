01f54769fd3927385e719781331279703071bd48
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-06 21:54:17 +0700

    Add files via upload

--- src/ndi-rosa.js
+++ src/ndi-rosa.js
@@ -3,7 +3,6 @@ import {React, useState} from 'react';
 import { Component } from 'react';
 import { useHistory } from 'react-router-dom';
 import './ndi-rosa.css';
-import e from 'express';
 
 export default class NDI extends Component{
   constructor(props) {
@@ -14,10 +13,6 @@ export default class NDI extends Component{
       prev: 0,
     };
   }
-    handleClick = e =>{
-        e.preventDefault();
-        this.props.history.push('/qtree');
-    }
     calculate=(ele)=>{
       if (ele.target.name!=this.state.memory){
       this.setState(previousState => ({
@@ -46,7 +41,7 @@ export default class NDI extends Component{
     render() {
         return(
             <div> 
-                <h1 class="mx-5 pb-3" id="demographic-data"> Neck Disability Index (NDI) </h1>
+                <h1 class="mx-5 pb-3" id="demographic-data1"> Neck Disability Index (NDI) </h1>
                 <p class="mx-5 p-3" id="instruction1"> 
                     แบบสอบถามนี้ทำขึ้นเพื่อรับข้อมูลว่าอาการปวดคอมีผลต่อการใช้ชีวิตประจำวันของท่านอย่างไร
                     กรุณาทำเครื่องหมายลงในช่องเพียงหนึ่งช่อง
@@ -55,7 +50,7 @@ export default class NDI extends Component{
 
 {/* คำถาม NDI */}      
 
-                <Form className="px-5" class="screening-form1">
+                <Form className="px-5" id="screening-form1">
 {/*Q1*/}            <Form.Group className="mb-3 px-4">
                         <Form.Label id="question1"> 1. ความเจ็บปวด </Form.Label>
                         {['radio'].map((type) => (
@@ -680,7 +675,7 @@ export default class NDI extends Component{
                 </Form>
                 <Button class="btn" id={0} type="submit" name="btn" onClick={this.calculate}>Finalize</Button>
                 <p id="Nxtbutton1">
-                    <Button class="btn" id="btn-login" type="submit" onClick={this.handleClick}><b>Next</b></Button>
+                    <Button class="btn" id="btn-login" type="submit" href="/rosa"><b>Next</b></Button>
                 </p>
 
             </div>
