ab8848766d4e3e7de8ca04c1c5e38cceee600a61
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-06 20:44:42 +0700

    Add files via upload

--- src/ndi-rosa.js
+++ src/ndi-rosa.js
@@ -41,7 +41,7 @@ export default class NDI extends Component{
     render() {
         return(
             <div> 
-                <h1 class="mx-5 pb-3" id="demographic-data"> Neck Disability Index (NDI) </h1>
+                <h1 class="mx-5 pb-3" id="demographic-data1"> Neck Disability Index (NDI) </h1>
                 <p class="mx-5 p-3" id="instruction1"> 
                     แบบสอบถามนี้ทำขึ้นเพื่อรับข้อมูลว่าอาการปวดคอมีผลต่อการใช้ชีวิตประจำวันของท่านอย่างไร
                     กรุณาทำเครื่องหมายลงในช่องเพียงหนึ่งช่อง
@@ -50,7 +50,7 @@ export default class NDI extends Component{
 
 {/* คำถาม NDI */}      
 
-                <Form className="px-5" class="screening-form1">
+                <Form className="px-5" id="screening-form1">
 {/*Q1*/}            <Form.Group className="mb-3 px-4">
                         <Form.Label id="question1"> 1. ความเจ็บปวด </Form.Label>
                         {['radio'].map((type) => (
@@ -646,7 +646,7 @@ export default class NDI extends Component{
                                 <Form.Check
                                     type={type}
                                     id={3}
-                                    name="ndi10"
+                                    name="ndi"
                                     label="ฉันสามารถเข้าร่วมกิจกรรมนันทนาการของฉันได้บ้างเพราะปวดคอ"
                                     onClick={this.calculate}
                                 />
@@ -673,9 +673,8 @@ export default class NDI extends Component{
                         ))}
                     </Form.Group>                
                 </Form>
-                <Button class="btn" id={0} type="submit" name="btn" onClick={this.calculate}>Finalize</Button>
                 <p id="Nxtbutton1">
-                    <Button class="btn" id="btn-login" type="submit" href="/qtree"><b>Next</b></Button>
+                    <Button class="btn" id={0} type="submit" onClick={this.calculate} href="/rosa"><b>Next</b></Button>
                 </p>
 
             </div>
