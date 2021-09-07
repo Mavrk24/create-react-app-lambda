6ddfa46ff36bbf3e265663abf6d264d0ef14c885
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-07 23:16:07 +0700

    Update recommendation.js

--- src/recommendation.js
+++ src/recommendation.js
@@ -49,7 +49,7 @@ export default class Recommendation extends Component{
         
     getTarget = () => {
         var target = localStorage.getItem('target');
-        console.log(target, randnum);
+        console.log(target, this.state.randnum);
         if (target=='neck'){
             var num = Math.floor(this.state.randnum*6) //สุ่มเลขตั้งแต่สร้างState
             const arr = [1,2,4,7,9]
