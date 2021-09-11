7bf813c359b1b7a4de5750d8b8884b18662e35b8
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-11 23:58:18 +0700

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -168,14 +168,14 @@ const byPass = (e) => {
                  
                 <a style={{color: "27AE60"}} href="/register" onClick={handleClick}>
                   <strong> New to euHabit? Register now. </strong> </a>
-                </ul>
-              </form>
-            </div>
-             <Form id="terms-checkbox">
+                 <Form id="terms-checkbox">
              <Form.Group controlId="formBasicCheckbox">
                     <Form.Check checked={checked} onClick={handleShow} type="checkbox" label="รับทราบและยินยอมปฎิบัติตามข้อตกลงและเงื่อนไขในการให้บริการของ euHabit" />
                   </Form.Group>
                 </Form>
+                </ul>
+              </form>
+            </div>
         </div>
         </div>
       </div>
