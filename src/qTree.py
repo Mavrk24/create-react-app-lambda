05838fd255f70ebe0a4e3ba00c61a9e4072fed7e
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-14 23:19:58 +0700

    Update qTree.py

--- src/qTree.py
+++ src/qTree.py
@@ -2,17 +2,17 @@ from anytree import Node, RenderTree,AsciiStyle,PreOrderIter
 from anytree.dotexport import RenderTreeGraph
 
 root = Node('root', value = 'null', label='') 
-a1 = Node('Do you use laptop for > 3 hours per day ?', parent=root, value='null',children=[
-    Node('Do you use laptop on sofa / on bed ?',  value='null',label='Have a life'),#5
-    Node('Do you use laptop on lap ?',  value='null',label='Have a life'), #6
-    Node('Do you have to look down on screen ?', value='null',label='Have a life')], label='') #7s
+a1 = Node('Do you use laptop for > 3 hours per day ?', parent=root, value='null',children=[ #1
+    Node('Do you use laptop on sofa / on bed ?',  value='null',label='Have a life'),#2
+    Node('Do you use laptop on lap ?',  value='null',label='Have a life'), #3
+    Node('Do you have to look down on screen ?', value='null',label='Have a life')], label='') #4s
 a2 = Node('Do you have to look while you type ?', parent=root , value='null',label=''), 
 b11 = Node('Do you use iPad/Tablet > 3 hours per day ?', parent=root, value='null',children=[
-    Node('Do you use iPad/Tablet on table / on lap (or bed) ?',  value='null',label='')], label='') #16
-b2 = Node('Do you use without standing cases ?', parent=root, value='null',label='') #17
+    Node('Do you use iPad/Tablet on table / on lap (or bed) ?',  value='null',label='')], label='') #7
+b2 = Node('Do you use without standing cases ?', parent=root, value='null',label='') #8
 c = Node('Do you do manual work (drawing, drafting, sketching) ?', parent=root, value='null',label='')
 c1 = Node('Do you do the work on a table', parent=c, value='null',label='')
-c11 = Node('Is it a slope table ?', parent=c1, value='null',label='') #24s
+c11 = Node('Is it a slope table ?', parent=c1, value='null',label='') #11s
 d = Node('Do you use a chair without arm rest ?', parent=root, value='null',label='')
 d1 = Node('Do you use a chair without lumbar back support ?', parent=d, value='null',label='')
 
